663fd99c98e0574c6ca4193a8ee016b8d0c3ef05
Immanuel Bayu <bayu@tiket.com>
2017-04-13 18:29:40 +0700

    Add Publish + Subscribe

--- tutorial/rabbitmq/publish.js
+++ tutorial/rabbitmq/publish.js
@@ -1,5 +1,26 @@
 const amqp = require('amqplib');
+const uuid = require('uuid/v4');
 
 const amqpCon = amqp.connect('amqp://localhost');
 
-amqpCon.createChannel();
+amqpCon
+  .then(conn => conn.createChannel())
+  .then((ch) => {
+    ch.assertQueue('', {
+      exclusive: true,
+      // expires: 5000,
+      // autodelete: true,
+    })
+    .then((q) => {
+      const corr = uuid();
+      const queue = 'test_pub_sub_rpc';
+      const content = 'Hello World';
+
+      ch.sendToQueue(queue, new Buffer(content), {
+        correlationId: corr,
+        replyTo: q.queue,
+      });
+
+      console.log('Sent Hello World');
+    });
+  });
--- tutorial/rabbitmq/subscribe.js
+++ tutorial/rabbitmq/subscribe.js
@@ -1,12 +1,16 @@
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
+// const uuid = require('uuid/v4');
 
-amqp.connect('amqp://localhost', function(err, conn){
-	conn.createChannel(function(err, ch){
-		var q = 'hello';
-		var msg = 'hello world';
-		ch.assertQueue(q, {durable: false});
-		ch.consume(q, function(msg){
-			console.log(" [x] Received %s", msg.content.toString());
-		});
-	}, {noAck: true});
-});
+const amqpCon = amqp.connect('amqp://localhost');
+
+amqpCon
+  .then(conn => conn.createChannel())
+  .then((ch) => {
+    const queue = 'test_pub_sub_rpc';
+    ch.assertQueue(queue, { durable: false });
+    ch.prefetch(1000);
+    ch.consume(queue, (msg) => {
+      const content = msg.content.toString();
+      console.log(content);
+    });
+  });
